76bc1132bccce0d162061ccfe1ff5c67efb28b67
Jacob Hocker <jacobdouglas06@gmail.com>
2023-01-25 12:38:53 -0600

    added code for server and the authmiddleware

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(express.json())
+app.use(cors())
+
+const db = require("./models")
+
+
+//! Routers
+const quizRouter = require('./routes/Quizs')
+app.use("/quizzes", quizRouter)
+const questionRouter = require('./routes/Questions')
+app.use("/questions", questionRouter)
+const userRouter = require('./routes/Users')
+app.use("/auth", userRouter)
+const catRouter = require('./routes/Cats')
+app.use("/categories", catRouter)
+const scoreRouter = require('./routes/Scores')
+app.use("/scores", scoreRouter)
+
+db.sequelize.sync().then(() => {
+        app.listen(2000, () => {
+        console.log("Server running on port 2000")
+    })
+})
