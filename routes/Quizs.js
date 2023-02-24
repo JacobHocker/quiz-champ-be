8f649919428e6bf5d7be15e04bb0beffada63e4a
Jacob Hocker <jacobdouglas06@gmail.com>
2023-02-24 12:32:51 -0600

    deleted a requirement

--- routes/Quizs.js
+++ routes/Quizs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Quizs, Sequelize } = require("../models");
+const { Quizs } = require("../models");
 
 
 router.get("/", async (req, res) => {
