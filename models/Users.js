f90f800cc9de19fc8ec811dd7fb33918a252e7a9
Jacob Hocker <jacobdouglas06@gmail.com>
2023-03-01 12:12:22 -0600

    added routes for the post functionality

--- models/Users.js
+++ models/Users.js
@@ -33,6 +33,12 @@ module.exports = (sequelize, DataTypes) => {
             onDelete: 'cascade',
         })
     }
+    Users.associate = (models) => {
+        Users.hasMany(models.Posts,{
+            onDelete: 'cascade',
+        })
+    }
+
 
     return Users;
 }
--- server.js
+++ server.js
@@ -12,21 +12,26 @@ const db = require("./models")
 
 //! Routers
 const quizRouter = require('./routes/Quizs')
-app.use("/quizzes", quizRouter)
-const questionRouter = require('./routes/Questions')
-app.use("/questions", questionRouter)
-const userRouter = require('./routes/Users')
-app.use("/auth", userRouter)
-const catRouter = require('./routes/Cats')
-app.use("/categories", catRouter)
-const scoreRouter = require('./routes/Scores')
-app.use("/scores", scoreRouter)
+app.use("/quizzes", quizRouter);
 
-// db.sequelize.sync().then(() => {
-//         app.listen(2000, () => {
-//         console.log("Server running on port 2000")
-//     })
-// })
+const questionRouter = require('./routes/Questions')
+app.use("/questions", questionRouter);
+
+const userRouter = require('./routes/Users')
+app.use("/auth", userRouter);
+
+const catRouter = require('./routes/Cats')
+app.use("/categories", catRouter);
+
+const scoreRouter = require('./routes/Scores')
+app.use("/scores", scoreRouter);
+
+const postRouter = require('./routes/Posts')
+app.use("/posts", postRouter);
+
+const commentRouter = require('./routes/Comments')
+app.use("/comments", commentRouter);
+
 db.sequelize
     .sync()
     .then(() => {
