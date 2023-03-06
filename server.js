f94a3cf5a4ca9a8f63abc7fc4412ec0bd697c5da
Jacob Hocker <jacobdouglas06@gmail.com>
2023-03-06 10:06:22 -0600

    disabled headers

--- server.js
+++ server.js
@@ -6,26 +6,15 @@ require("dotenv").config();
 
 app.use(express.json())
 app.use(cors(
-    { 
-    credentials: true 
-    },
-    {
-        origin: ['https://quiz-champ.onrender.com', 'https://quiz-champ-be.herokuapp.com/']
-    }
+    // { 
+    // credentials: true 
+    // },
+    // {
+    //     origin: ['https://quiz-champ.onrender.com', 'https://quiz-champ-be.herokuapp.com/']
+    // }
     
     ))
-    app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept"
-        );
-        res.setHeader(
-            "Access-Control-Allow-Methods",
-            "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-        );
-        next();
-    });
+    
 
 const db = require("./models")
 
